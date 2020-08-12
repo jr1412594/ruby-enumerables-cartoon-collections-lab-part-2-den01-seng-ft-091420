d494f576942597d3b6c14c324c1c913d8aad2bf3
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-12 01:59:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.each do |element|
     if element == valid_calls
-      return element
   end
+  return element
 end
